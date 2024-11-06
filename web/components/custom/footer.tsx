2f5bca0ab8de172877dbf3aa195eb3c18ef62677
Samy AMINE <samyaminework@gmail.com>
2024-11-06 20:05:13 +0100

    FEATURE: footer

--- web/components/custom/footer.tsx
+++ web/components/custom/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className={`w-full px-24 py-10 bg-black text-white flex justify-between items-center`}>
             <div>
                 <p className={`text-lg`}>
-                    Made by Samy Amine
+                    Made by Ouiam & Samy
                 </p>
                 <nav className={`mt-3`}>
                     <ul>
