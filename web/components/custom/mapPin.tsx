fdc2fc369a394c6ffafec3de021e88cd5e5750b6
Samy AMINE <samyaminework@gmail.com>
2024-11-19 21:48:50 +0100

    COMPONENT: map pin

---
+++ web/components/custom/mapPin.tsx
@@ -0,0 +1,8 @@
+
+export default function MapPin() {
+    return (
+        <div className={`w-3 h-3 rounded-full bg-main`}>
+            <div className={`w-3 h-3 rounded-full bg-main animate-ping`}></div>
+        </div>
+    );
+}
