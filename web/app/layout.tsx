24ebc81abb2c55a75d66837c206086b40895c7d2
Samy AMINE <samyaminework@gmail.com>
2024-11-21 22:02:33 +0100

    FEATURE: Global responsive horizontal padding

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -77,8 +77,8 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
                 <title>Glance | Your personal AI fashion shopper</title>
             </head>
             <body className={`${ttHovesProTrial.className} text-black`}>
-            <Header />
-                <div className={`w-full flex flex-col items-center px-24`}>
+                <Header />
+                <div className={`flex flex-col items-center px-4 md:px-16 lg:px-24`}>
                     {children}
                 </div>
                 <Footer />
