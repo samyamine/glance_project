d1250d3a64e03c42d72f41b5a36daaafa4da69ad
Samy AMINE <samyaminework@gmail.com>
2024-11-21 21:34:42 +0100

    FEATURE: Responsive Interactive curation and selection

--- web/components/custom/featureText.tsx
+++ web/components/custom/featureText.tsx
@@ -1,7 +1,7 @@
 
 export default function FeatureText({ title, description }: { title: string; description: string }) {
     return (
-        <div className={`relative top-1/2 -translate-y-1/2`}>
+        <div className={`md:relative md:top-1/2 md:-translate-y-1/2`}>
             <h3 className={`mb-10 text-3xl font-bold`}>
                 {title}
             </h3>
