0bc1991869c712a43286daec5a4c6235287ecf7b
Samy AMINE <samyaminework@gmail.com>
2024-11-25 22:17:29 +0100

    FEATURE: Outfits

--- web/components/custom/heroImages.tsx
+++ web/components/custom/heroImages.tsx
@@ -6,22 +6,40 @@ import {getImageSamples} from "@/lib/utils";
 
 export default function HeroImages() {
     const number_images = 4;
-    const [images, setImages] = useState<Array<Array<string>>>([
-        ["/clothes/shoes_men_1.webp"],
-        ["/clothes/jeans_women_1.webp"],
-        ["/clothes/top_women_4.webp"],
-        ["/clothes/shoes_men_3.webp"],
-    ]);
+
+    // FIXME: Set images
+    // const [images, setImages] = useState<Array<Array<string>>>([
+    //     ["/clothes/shoes_men_1.webp"],
+    //     ["/clothes/jeans_women_1.webp"],
+    //     ["/clothes/top_women_4.webp"],
+    //     ["/clothes/shoes_men_3.webp"],
+    // ]);
+
+    // FIXME: Finish outfits
+    const images: Array<Array<string>> = [
+    ["/outfits/outfit_1_1.webp", "/outfits/outfit_2_1.jpg", "/outfits/outfit_3_1.jpg", "/outfits/outfit_4_1.avif", "/outfits/outfit_5_1.jpg", "/outfits/outfit_6_1.webp", "/outfits/outfit_7_1.avif", "/outfits/outfit_8_1.webp", "/outfits/outfit_9_1.webp", "/outfits/outfit_10_1.webp"],
+        ["/outfits/outfit_1_2.webp", "/outfits/outfit_2_2.webp", "/outfits/outfit_3_2.jpg", "/outfits/outfit_4_2.avif", "/outfits/outfit_5_2.jpg", "/outfits/outfit_6_2.webp", "/outfits/outfit_7_2.avif", "/outfits/outfit_8_2.webp", "/outfits/outfit_9_2.webp", "/outfits/outfit_10_2.webp"],
+        ["/outfits/outfit_1_3.webp", "/outfits/outfit_2_3.webp", "/outfits/outfit_3_3.jpg", "/outfits/outfit_4_3.webp", "/outfits/outfit_5_3.jpg", "/outfits/outfit_6_3.webp", "/outfits/outfit_7_3.webp", "/outfits/outfit_8_3.webp", "/outfits/outfit_9_3.webp", "/outfits/outfit_10_3.webp"],
+        ["/outfits/outfit_1_4.webp", "/outfits/outfit_2_4.jpg", "/outfits/outfit_3_4.jpg", "/outfits/outfit_4_4.webp", "/outfits/outfit_5_4.webp", "/outfits/outfit_6_4.webp", "/outfits/outfit_7_4.webp", "/outfits/outfit_8_4.webp", "/outfits/outfit_9_4.webp", "/outfits/outfit_10_4.webp"],
+    ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-        if (images[0].length === 1) {
-            setImages(getImageSamples(number_images));
-        }
+    // useEffect(() => {
+    //     if (images[0].length === 1) {
+    //         setImages(getImageSamples(number_images));
+    //     }
+    //
+    //     const interval = setInterval(() => {
+    //         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    //     }, 3000);
+    //
+    //     return () => clearInterval(interval);
+    // }, [images]);
 
+    useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % images[0].length);
         }, 3000);
 
         return () => clearInterval(interval);
