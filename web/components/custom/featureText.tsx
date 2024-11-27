5e039fb437c62719558adaa53b6b4e577c6581da
Samy AMINE <samyaminework@gmail.com>
2024-11-27 23:06:43 +0100

    FIX: text position according to screen size. Previous commit is main page imports cleaning

--- web/components/custom/featureText.tsx
+++ web/components/custom/featureText.tsx
@@ -3,7 +3,7 @@ export default function FeatureText({ title, description }: { title: string; des
     return (
         // <div className={`md:relative md:top-1/2 md:-translate-y-1/2 max-md:text-center`}>
         <div className={`md:relative md:top-1/2 md:-translate-y-1/2`}>
-            <h3 className={`mb-10 text-3xl font-bold text-center`}>
+            <h3 className={`mb-10 text-3xl font-bold max-md:text-center`}>
                 {title}
             </h3>
             <p className={`text-lg`}>
